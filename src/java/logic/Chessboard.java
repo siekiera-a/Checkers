73392ccebd748b9c10a42491888ccc7ce18388e5
siekiera-a <siekiera.a31@gmail.com>
2020-05-23 14:12:33 +0200

    Added new methods to Chessboard class

--- src/java/logic/Chessboard.java
+++ src/java/logic/Chessboard.java
@@ -10,12 +10,22 @@ public class Chessboard {
         board = createEmptyBoard();
     }
 
+    /**
+     * Create chessboard
+     *
+     * @return new chessboard with placed pawns
+     */
     public static Chessboard createChessboard() {
         return new Chessboard();
     }
 
-    // left top corner field is empty
+    /**
+     * Place black pawns on top, white pawns on bottom
+     *
+     * @return array with placed pawns
+     */
     private Pawn[][] createEmptyBoard() {
+        // left top corner field is empty
         Pawn[][] board = new Pawn[BOARD_HEIGHT][BOARD_WIDTH];
 
         // place black pawns
@@ -33,6 +43,77 @@ public class Chessboard {
         }
 
         return board;
+    }
+
+    private boolean fieldOutOfBoard(Position pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+
+        return x < 0 || x >= BOARD_WIDTH || y < 0 || y >= BOARD_HEIGHT;
+    }
+
+    /**
+     * Check if position exists on the board and is empty
+     */
+    public boolean fieldEmpty(Position pos) {
+        return pos != null && !fieldOutOfBoard(pos) && board[pos.getY()][pos.getX()] == null;
+    }
+
+    /**
+     * Get pawn position on the board
+     *
+     * @return If pawn is on the board return its position, otherwise null
+     */
+    public Position getPawnPosition(Pawn pawn) {
+        if (pawn == null) {
+            return null;
+        }
+
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            for (int column = 0; column < BOARD_WIDTH; column++) {
+                if (board[row][column] == pawn) {
+                    return new Position(column, row);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Remove pawn from the board
+     */
+    public void removePawn(Pawn pawn) {
+        Position pawnPos = getPawnPosition(pawn);
+
+        if (pawnPos != null) {
+            board[pawnPos.getY()][pawnPos.getX()] = null;
+        }
+    }
+
+    /**
+     * Return pawn on given position
+     *
+     * @return Pawn or null
+     */
+    public Pawn getPawn(Position pos) {
+        if (pos == null || fieldOutOfBoard(pos)) {
+            return null;
+        }
+
+        return board[pos.getY()][pos.getX()];
+    }
+
+    /**
+     * Move pawn at new position if exists and is on the board
+     */
+    public void movePawn(Pawn pawn, Position newPosition) {
+        Position currentPosition = getPawnPosition(pawn);
+
+        if (currentPosition != null && fieldEmpty(newPosition)) {
+            board[newPosition.getY()][newPosition.getX()] = pawn;
+            board[currentPosition.getY()][currentPosition.getX()] = null;
+        }
     }
 
 }
