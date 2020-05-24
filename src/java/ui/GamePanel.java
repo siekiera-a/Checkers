469b99ec78ca2e58fe541e179660ee8c5aa8b7d9
siekiera-a <siekiera.a31@gmail.com>
2020-05-25 00:23:00 +0200

    Added handle user interactions and drawing chessboard with helpers

--- src/java/ui/GamePanel.java
+++ src/java/ui/GamePanel.java
@@ -1,51 +1,93 @@
 package ui;
 
 import logic.Field;
-import logic.GameController;
+import logic.Move;
 import logic.Player;
+import logic.Position;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.List;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements MouseListener {
 
     private final Color blackKingFill = new Color(35, 35, 35);
     private final Color whiteKingFill = new Color(190, 190, 190);
-    private final float fillPercentage = 0.7f;
     private final int padding = 5;
     private int fieldWidth;
     private int fieldHeight;
-    private GameController game;
+    private final static int BOARD_WIDTH = 8;
+    private final static int BOARD_HEIGHT = 8;
+    private final Color helperColor = Color.GREEN;
+    private PlayerController playerController;
 
     public GamePanel(int width, int height) {
         fieldWidth = width / 8;
         fieldHeight = height / 8;
 
-        game = GameController.startNewGame(Player.WHITE);
+        playerController = new PlayerController(fieldWidth, fieldHeight);
         setPreferredSize(new Dimension(width, height));
+        addMouseListener(this);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Field[][] fields = game.getFields();
+        Field[][] fields = playerController.getFields();
+        drawChessboard(g);
+        drawAvailableMoves(playerController.getAvailableMoves(), g);
+        Move activeMove = playerController.getActive();
+        if (activeMove != null) {
+            drawActiveMove(activeMove, g);
+        }
         drawCheckers(fields, g);
+    }
+
+    private void drawActiveMove(Move move, Graphics g) {
+        Position active = move.getStartPosition();
+        drawRect(active.getY(), active.getX(), helperColor, g);
+        move.getAvailablePositions()
+            .forEach(pos -> drawRect(pos.getY(), pos.getX(), helperColor, g));
+    }
+
+    private void drawAvailableMoves(List<Move> moves, Graphics g) {
+        moves.forEach(move -> drawBorder(move.getStartPosition(), helperColor, g));
+    }
+
+    private void drawChessboard(Graphics g) {
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            for (int column = 0; column < BOARD_WIDTH; column++) {
+                Color color = (row + column) % 2 == 0 ? Color.LIGHT_GRAY : Color.GRAY;
+                drawRect(row, column, color, g);
+            }
+        }
     }
 
     private void drawCheckers(Field[][] fields, Graphics g) {
         for (int row = 0; row < fields.length; row++) {
             for (int column = 0; column < fields[row].length; column++) {
-                Color color = (row + column) % 2 == 0 ? Color.LIGHT_GRAY : Color.GRAY;
-                drawRect(row, column, color, g);
                 Field field = fields[row][column];
                 if (field != Field.EMPTY) {
                     drawField(row, column, field, g);
                 }
             }
         }
+    }
+
+    private void drawBorder(Position position, Color color, Graphics g) {
+        int[] coordinates = calcPositions(position.getY(), position.getX());
+        int thickness = 3;
+        ((Graphics2D) g).setStroke(new BasicStroke(thickness));
+        g.setColor(color);
+        g.drawRect(coordinates[0], coordinates[1], fieldWidth - 1, fieldHeight - 1);
     }
 
     /***
@@ -99,6 +141,7 @@ public class GamePanel extends JPanel {
     private void drawKing(int x, int y, Color color, Graphics g) {
         drawPawn(x, y, color, g);
 
+        float fillPercentage = 0.7f;
         int width = (int) ((fieldWidth - 2 * padding) * fillPercentage);
         int height = (int) ((fieldHeight - 2 * padding) * fillPercentage);
 
@@ -106,5 +149,32 @@ public class GamePanel extends JPanel {
 
         g.setColor(c);
         g.fillOval(x + (fieldWidth - width) / 2, y + (fieldHeight - height) / 2, width, height);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        playerController.handleClick(e);
+        repaint();
+        if (playerController.gameEnded()) {
+            String player = playerController.whoseTurn() == Player.BLACK ? "White" : "Black";
+            JOptionPane.showMessageDialog(null, player + " won!");
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
---
+++ src/java/ui/PlayerController.java
@@ -0,0 +1,126 @@
+package ui;
+
+import logic.Field;
+import logic.GameController;
+import logic.Move;
+import logic.Player;
+import logic.Position;
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerController {
+
+    private int fieldWidth;
+    private int fieldHeight;
+    private GameController game;
+    private Position lastClickedPawnPosition;
+    private List<Move> moves;
+    private Move obligatoryMove;
+
+    public PlayerController(int fieldWidth, int fieldHeight) {
+        this.fieldWidth = fieldWidth;
+        this.fieldHeight = fieldHeight;
+        game = GameController.startNewGame(Player.WHITE);
+    }
+
+    /**
+     * Get active move
+     *
+     * @return active move or null if there is no active move
+     */
+    public Move getActive() {
+        if (lastClickedPawnPosition == null) {
+            return null;
+        }
+
+        if (obligatoryMove != null) {
+            return obligatoryMove;
+        }
+
+        return moves.stream()
+            .filter(move -> move.getStartPosition().equals(lastClickedPawnPosition))
+            .findFirst()
+            .orElse(null);
+    }
+
+    public Player whoseTurn() {
+        return game.whoseTurn();
+    }
+
+    /**
+     * @return list of available moves
+     */
+    public List<Move> getAvailableMoves() {
+        if (obligatoryMove != null) {
+            List<Move> list = new ArrayList<>();
+            list.add(obligatoryMove);
+            return list;
+        }
+
+        return game.getMoves();
+    }
+
+    public boolean gameEnded() {
+        return getAvailableMoves().isEmpty();
+    }
+
+    public void handleClick(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        Position pos = getPositionOnBoard(x, y);
+
+        moves = game.getMoves();
+
+        moves.stream()
+            .filter(move -> move.getStartPosition().equals(pos))
+            .findFirst()
+            .ifPresentOrElse(
+                move -> lastClickedPawnPosition = move.getStartPosition(),
+                () -> handleAction(pos));
+    }
+
+    /**
+     * Move or jump pawn
+     */
+    private void handleAction(Position position) {
+        moves.stream()
+            .filter(move -> move.getStartPosition().equals(lastClickedPawnPosition))
+            .findFirst()
+            .ifPresent(move -> {
+                if (move.getAvailablePositions().contains(position)) {
+                    if (move.captureMoves()) {
+                        Move nextMove = game.jump(lastClickedPawnPosition, position);
+                        if (nextMove.captureMoves()) {
+                            obligatoryMove = nextMove;
+                            lastClickedPawnPosition = position;
+                        } else {
+                            obligatoryMove = null;
+                            lastClickedPawnPosition = null;
+                        }
+                    } else {
+                        game.move(lastClickedPawnPosition, position);
+                        obligatoryMove = null;
+                        lastClickedPawnPosition = null;
+                    }
+                }
+            });
+    }
+
+    /**
+     * get
+     *
+     * @return calculated position
+     */
+    private Position getPositionOnBoard(int x, int y) {
+        int row = y / fieldHeight;
+        int column = x / fieldWidth;
+        return new Position(column, row);
+    }
+
+    public Field[][] getFields() {
+        return game.getFields();
+    }
+
+}
