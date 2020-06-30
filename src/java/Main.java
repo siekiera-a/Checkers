29a025382e6152e11b7a1f9c466cd2cc4caede46
siekiera-a <siekiera.a31@gmail.com>
2020-06-30 23:43:50 +0200

    Added maven support

--- src/java/Main.java
+++ src/java/Main.java
@@ -3,7 +3,7 @@ import ui.Frame;
 public class Main {
 
     public static void main(String[] args) {
-        Frame frame = new Frame("Checkers", 800, 800);
+        new Frame("Warcaby", 800, 800);
     }
 
 }
