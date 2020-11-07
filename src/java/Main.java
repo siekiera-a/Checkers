968c3c981d0a550025d5f70af458c79be1be3f4c
siekiera-a <siekiera.a31@gmail.com>
2020-11-07 18:02:59 +0100

    App won't throw exception in headless environment

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,9 +1,15 @@
 import ui.Frame;
 
+import java.awt.HeadlessException;
+
 public class Main {
 
     public static void main(String[] args) {
-        new Frame("Warcaby", 800, 800);
+        try {
+            new Frame("Warcaby", 800, 800);
+        } catch (HeadlessException e) {
+            System.out.println("Headless environment detected! Couldn't start the app!");
+        }
     }
 
 }
